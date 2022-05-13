ef11c2e0ec8869baafa5955e093790a5337b518b
Benjamin Herald <bherald@pm.me>
2022-05-14 00:49:22 +0300

    price2

--- src/app.jl
+++ src/app.jl
@@ -47,7 +47,7 @@ route("/") do
 end
 
 route("/", method = POST) do
-  cf = [0-price,10,10,10,10,10,10,10,10,10,10,10,400]	
+  cf = [0-postpayload(:price, 500),10,10,10,10,10,10,10,10,10,10,10,400]	
   result = xirr(cf,dates)	
   "XIRR for price $(postpayload(:price, "test")) is $(result)"
    html(form)
