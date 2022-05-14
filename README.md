ccff46fb52f88ad59812d2400d962f2e83e4f67d
Benjamin Herald <bherald@pm.me>
2022-05-14 15:02:51 +0300

    profiling

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Julia XIRR
 Calculating XIRR with Julia on web and with POST API on heroku server
 
+## Deployment
+
+web https://ancient-sea-xirr2.herokuapp.com
+api https://ancient-sea-xirr2.herokuapp.com/xirr 
+api JSON format: {"price": "500", "deposit": "104"}
+
+
+
 ## Links
 
 https://discourse.julialang.org/t/optimization-using-optim-and-roots/39301
--- src/app.jl
+++ src/app.jl
@@ -14,7 +14,8 @@ function xirr(cf,dates)
     f(x) = xnpv(x,cf,interval)
     result = optimize(x -> f(x)^2,0.0,1.0,Brent())
     println("optimize result is $(result)")
-    return result.minimizer
+#    return result.minimizer
+    return result
 end
 
 dates = Date(2022,01,01):Month(1):Date(2023,01,01)
@@ -55,23 +56,27 @@ route("/xirr", method = POST) do
   params = jsonpayload()
   price = parse(Int64, params["price"])
   deposit = parse(Int64, params["deposit"])
+  start = time_ns();
   firstcf = deposit - price
   cf = [firstcf,10,10,10,10,10,10,10,10,10,10,10,400]	
   result = xirr(cf,dates)	  
-  msg = "XIRR for price $(price) and deposit $(deposit) is $(result)"
+  duration = (time_ns() - start) / 1000000
+  msg = "XIRR for price $(price) and deposit $(deposit) is $(result.minimizer)"
   println("$(msg)")
 #  println("cf are $(cf)")
 
-  (:xirr => result) |> json
+  (:xirr => result.minimizer, :duration_ms => duration , :log => result ) |> json
 end
 
 route("/", method = POST) do
   price = parse(Int64, postpayload(:price))
   deposit = parse(Int64, postpayload(:deposit))
+  start = time_ns();
   firstcf = deposit - price
   cf = [firstcf,10,10,10,10,10,10,10,10,10,10,10,400]	
-  result = xirr(cf,dates)	
-  msg = "XIRR for price $(price) and deposit $(deposit) is $(result)"
+  result = xirr(cf,dates)
+  duration = (time_ns() - start) / 1000000	
+  msg = "XIRR for price $(price) and deposit $(deposit) is $(result.minimizer), calculated in $(duration) ms"
   
  newform = """
 <h3>$(msg)</h3>
@@ -87,6 +92,7 @@ route("/", method = POST) do
    <div>
   <input type="submit" value="Calculate new XIRR" />
   </div>
+    $(result)
 </form>
 """ 
 
